603be9ce4e5f1b7c3b2c5d2bba55afdae5b8f0a3
Robin Smit <robinsmit@gmail.com>
2016-09-08 12:26:48 +0200

    commit

--- src/main/java/nl/deTesters/googledays/soaptest/RubenWebService.java
+++ src/main/java/nl/deTesters/googledays/soaptest/RubenWebService.java
@@ -2,12 +2,11 @@ package nl.deTesters.googledays.soaptest;
 
 import io.restassured.response.Response;
 import nl.deTesters.googledays.soaptest.restassured.WebServiceApplication;
-import org.junit.Assert;
 
 import java.util.UUID;
 
 public class RubenWebService extends WebServiceApplication {
-    public static final String BASE_URL = "http://192.168.1.58:9000/api";
+    public static final String BASE_URL = "http://192.168.1.58:9000/api/messagebox/";
 
     public RubenWebService() {
         super(BASE_URL);
